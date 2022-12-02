650fdcb6c0fdf3032ad2d99b18eb3a030a272dc4
Amit Kossnovic <amitkozo5528@gmail.com>
2022-12-02 14:23:14 +0200

    kozo adding cooments to Card.cpp

--- Card.cpp
+++ Card.cpp
@@ -1,54 +1,73 @@
 #include "Card.h"
 
-
+//Constructor card. evrey object get his type and his structs of stats.
+//The copy constructor , the destructor and the assignment operator is card.h.
 Card::Card(CardType type, const CardStats& stats) :
                     m_effects(type),
                     m_stats(stats)
                     {}
 
-void Card::applyEncounter(Player& player) const{
-    if(m_effects == CardType::Buff){ //
+//Getting the card type and make the turn according to his type and player stats.
+void Card::applyEncounter(Player& player) const
+{
+    if(m_effects == CardType::Buff)
+    { 
         player.buff(m_stats.buff);
 
     }
-    else if(m_effects == Battle){ //
+    else if(m_effects == Battle)
+    { 
     bool res;
-        if(player.getAttackStrength()>= m_stats.force){
+        if(player.getAttackStrength()>= m_stats.force)
+        {
            player.addCoins(m_stats.loot);
            player.levelUp(); 
            res = true;
         }
-        else{
+        else
+        {
             player.damage(m_stats.hpLoss);
             res = false;
         }
         printBattleResult(res);
     }  
-    else if(m_effects == CardType::Heal){ //
+    else if(m_effects == CardType::Heal)
+    { 
         player.heal(m_stats.heal);
     }
-    else if(m_effects == CardType::Treasure){ //
+    else if(m_effects == CardType::Treasure)
+    { 
         player.addCoins(m_stats.loot);
     }
-    else{//Shouldn't get here
+    else
+    {//Shouldn't get here
         return;
     }      
 }
 
-void Card::printInfo() const{
-     if(m_effects == CardType::Buff){ //
+
+//printing card data.
+void Card::printInfo() const
+{
+     if(m_effects == CardType::Buff)
+    { //
           printBuffCardInfo(m_stats);
     }
-    else if(m_effects == Battle){ //
+    else if(m_effects == Battle)
+    { //
           printBattleCardInfo(m_stats);
     }  
-    else if(m_effects == CardType::Heal){ //
+    else if(m_effects == CardType::Heal)
+    { //
           printHealCardInfo(m_stats);
     }
-    else if(m_effects == CardType::Treasure){ //
+    else if(m_effects == CardType::Treasure)
+    { //
           printTreausureCardInfo(m_stats);
     }
     else{//Shouldn't get here
         return;
     }      
-}                                    
+}        
+
+//**************** AMIT ADD THE LATES COMMENT IN 2.12.22 *******************//
